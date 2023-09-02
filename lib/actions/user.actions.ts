711ca98ba3dc64e08e81b2f9858444d84eebbb17
Pruthvi-Parade <parade.pruthvi@gmail.com>
2023-09-02 15:47:26 +0530

    #9 added a path

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,2 +1,37 @@
 "use server"
 
+import { revalidatePath } from "next/cache";
+import User from "../models/user.models";
+import { connectToDB } from "../mongoose"
+
+ export async function updateUser(
+    userId: string,
+    username: string,
+    name: string,
+    bio: string,
+    image: string,
+    path: string,
+    ): Promise<void> {
+    connectToDB();
+
+    try {
+        await User.findOneAndUpdate(
+            { id: userId },
+            {
+                username: username.toLowerCase(),
+                name,
+                bio,
+                image,
+                onboarded: true,
+            },
+            { upsert: true }
+        );
+
+        if( path == '/profile/edit') {
+            revalidatePath(path);
+        }
+    } catch (error) {
+        throw new Error(`Failed to create/update user: ${error}`);
+        
+    }
+ }
