f79e18f53751eda3063a01666a1ce34d943daf8c
Pruthvi-Parade <parade.pruthvi@gmail.com>
2023-09-05 16:09:33 +0530

    #9 Done with basic backend connection and post thread UI

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -44,3 +44,19 @@ export async function updateUser({
     throw new Error(`Failed to create/update user: ${error.message}`);
   }
 }
+
+export async function fetchUser(userId: string) {
+  try {
+    connectToDB();
+
+    return await User
+      .findOne({ id: userId })
+      // .populate({
+      //   path: 'communities',
+      //   model: Community 
+      // })
+  } catch (error: any) {
+    throw new Error(`Failed to fetch User: ${error.message}`);
+    
+  }
+}
